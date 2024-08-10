5f79c3abe68629e9eaeedf051246143f30491cd6
Gliese <123895160+Gliese832B@users.noreply.github.com>
2024-08-10 15:57:58 +0300

    WorldMaps and ItemInfo fixs

--- regrowtopia2/proton/shared/WorldMaps.cpp
+++ regrowtopia2/proton/shared/WorldMaps.cpp
@@ -1,1 +1,8 @@
 #include "WorldMaps.hpp"
+void* __fastcall WorldObject::Serialize(unsigned __int8* a2, int a3, int a4) {
+	bool v4 = a4 == 0;
+
+	MemorySerialize<int>(field_10, a2, a3, v4);
+
+	
+}
--- regrowtopia2/proton/shared/WorldMaps.hpp
+++ regrowtopia2/proton/shared/WorldMaps.hpp
@@ -1,5 +1,25 @@
 #pragma once
 #include "PlatformPrecomp.h"
+class WorldObject {
+public:
+
+	void* __fastcall Serialize(unsigned __int8* a2, int a3, int a4);
+private:
+	int field_0;
+	int field_4;
+	float field_8;
+	float field_C;
+	int field_10;
+};
+class WorldObjectMap {
+private:
+	int field_0;
+	int field_4;
+	int field_8;
+	int field_C;	
+public:
+
+};
 class WorldTileMap {
 private:
 	int field_0;
